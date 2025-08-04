965cc7264a7adc1c09389196330b26f539a0ce25
chizh273 <chizh273@gmail.com>
2025-08-04 20:00:15 +0300

    setup next-js

---
+++ drupal11-next/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        // protocol: 'https',
+        hostname: process.env.NEXT_IMAGE_DOMAIN,
+        // port: '',
+        pathname: '/sites/default/files/**',
+      },
+
+      {
+        // protocol: 'https',
+        hostname: 'localhost',
+        // port: '',
+        pathname: '/sites/default/files/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
